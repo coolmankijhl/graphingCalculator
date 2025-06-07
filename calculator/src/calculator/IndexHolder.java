4bf7278a45a0b30d5cd87aab449cd8904e408c87
beert <mucartwright@gmail.com>
2025-06-06 20:10:24 -0500

    update with executable

--- calculator/src/calculator/IndexHolder.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class IndexHolder {
-    public int index;
-
-    public IndexHolder(int index) {
-        this.index = index;
-    }
-}
--- execute.sh
+++ execute.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
 set -e
-mkdir -p bin
 javac -d bin src/*.java
-java -cp bin GraphingCalculator
+java -cp bin calculator.GraphingCalculator
 
